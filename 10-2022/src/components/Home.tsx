5eed6e7b0c72f1246ac1f0fb490441d983a850d9
Israel Mendes <101142371+MendesIsrael@users.noreply.github.com>
2022-10-22 10:22:59 -0300

    atual

--- 10-2022/src/components/Home.tsx
+++ 10-2022/src/components/Home.tsx
@@ -1,6 +1,8 @@
+import styles from './Home.module.css'
+
 export function Home() {
     return (
-        <div>
+        <div className={styles.bodyHome}>
             <p>
                 Somos a equipe RC Arkhi Design, uma empresa voltada em projetos de design de interiores, podendo elaborar móveis planejados, iluminação e decoração de ambientes.
                 Nosso time é composto por dois fundadores e integrantes: Christian Figueiredo, formado no curso Técnico de Edificações – que, inclusive, dá aula de Sketchup e Vray - e Raíssa de Sá, também formada em Edificações.
