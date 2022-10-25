8ec479708af35b8ac48d5ce980af86f7649e205d
Israel Mendes <101142371+MendesIsrael@users.noreply.github.com>
2022-10-25 08:14:22 -0300

    1

--- 10-2022/src/components/Page01.tsx
+++ 10-2022/src/components/Page01.tsx
@@ -16,7 +16,11 @@ export function Page01() {
             </nav>
             <div className={styles.mainImage}>
                 <img src="./src/img/arkhi-design.png" alt="arkhi design" />
-                <video src="./src/img/main-video.mp4" autoPlay loop muted/>
+                <video src="./src/img/main-video.mp4" autoPlay loop muted preload='./src/img/main-img.jpg'/>
+            </div>
+            <div className={styles.text}>
+                Somos a equipe RC Arkhi Design, uma empresa voltada em projetos de design de interiores, podendo elaborar móveis planejados, iluminação e decoração de ambientes.
+                Nosso time é composto por dois fundadores e integrantes: Christian Figueiredo, formado no curso Técnico de Edificações – que, inclusive, dá aula de Sketchup e Vray - e Raíssa de Sá, também formada em Edificações.
             </div>
             <div className={styles.carousel}>
                 <Carousel pause="hover">
