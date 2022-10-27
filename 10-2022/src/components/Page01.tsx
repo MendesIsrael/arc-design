ef671f468680625e9021b93ae6a162d2c34038cf
Israel Mendes <101142371+MendesIsrael@users.noreply.github.com>
2022-10-26 23:46:19 -0300

    atual

--- 10-2022/src/components/Page01.tsx
+++ 10-2022/src/components/Page01.tsx
@@ -3,7 +3,6 @@ import { Carousel } from 'react-bootstrap'
 import styles from './Page01.module.css'
 
 
-
 export function Page01() {
     return (
         <div className={styles.bodyPage01}>
@@ -12,11 +11,12 @@ export function Page01() {
                     <li><a href="#">serviço</a></li>
                     <li><a href="#">sobre</a></li>
                     <li><a href="#">departamento</a></li>
-                    <li><a href="#">contato</a></li>
+                    <li><a href="https://linkr.bio/rcarkhidesign" target="_blank">contato</a></li>
             </nav>
             <div className={styles.mainImage}>
                 <img src="./src/img/arkhi-design.png" alt="arkhi design" />
                 <video src="./src/img/main-video.mp4" autoPlay loop muted preload='./src/img/main-img.jpg'/>
+                <h1>Experiencias visuais de realidade máxima para o mercado imobiliário</h1>
             </div>
             <div className={styles.text}>
                 Somos a equipe RC Arkhi Design, uma empresa voltada em projetos de design de interiores, podendo elaborar móveis planejados, iluminação e decoração de ambientes.
