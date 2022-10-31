9df68171d31c9fcbf3e901b1c768f2a2669faadc
Israel Mendes <101142371+MendesIsrael@users.noreply.github.com>
2022-10-31 17:33:09 -0300

    0

--- 10-2022/src/components/RoutePages.tsx
+++ 10-2022/src/components/RoutePages.tsx
@@ -1,7 +1,7 @@
 import { Routes, Route } from 'react-router-dom'
 import { Page01 } from './Page01'
-import Servico from './Servico'
-import Sobre from './Sobre'
+import { Servico } from './Servico'
+import { Sobre } from './Sobre'
 
 export default function RoutePages() {
   return (
