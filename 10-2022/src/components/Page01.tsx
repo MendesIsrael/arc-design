aed36dda33e280bb42ba9e92ef32696ec4eca29d
Israel Mendes da Silva <israel.mendes97@hotmail.com>
2023-11-07 07:33:50 -0300

    texto

--- 10-2022/src/components/Page01.tsx
+++ 10-2022/src/components/Page01.tsx
@@ -11,39 +11,48 @@ export function Page01() {
             </div>
             <div className={styles.feed}>
             <div className={styles.feedText}>
-                    gfhjgyukuykjgykjg
+                    Somos a equipe RC Arkhi Design, uma empresa voltada em projetos de design de interiores, podendo elaborar móveis planejados, iluminação e decoração de ambientes.
+          Nosso time é composto por dois fundadores e integrantes: Christian Figueiredo, formado no curso Técnico de Edificações – que, inclusive, dá aula de Sketchup e Vray - e Raíssa de Sá, também formada em Edificações.
                 </div>
                 <div className={styles.feedImg1}></div>
                 <div className={styles.feedText}>
-                    gfhjgyukuykjgykjg
+                    Somos a equipe RC Arkhi Design, uma empresa voltada em projetos de design de interiores, podendo elaborar móveis planejados, iluminação e decoração de ambientes.
+          Nosso time é composto por dois fundadores e integrantes: Christian Figueiredo, formado no curso Técnico de Edificações – que, inclusive, dá aula de Sketchup e Vray - e Raíssa de Sá, também formada em Edificações.
                 </div>
                 <div className={styles.feedImg2}></div>
                 <div className={styles.feedText}>
-                    gfhjgyukuykjgykjg
+                    Somos a equipe RC Arkhi Design, uma empresa voltada em projetos de design de interiores, podendo elaborar móveis planejados, iluminação e decoração de ambientes.
+          Nosso time é composto por dois fundadores e integrantes: Christian Figueiredo, formado no curso Técnico de Edificações – que, inclusive, dá aula de Sketchup e Vray - e Raíssa de Sá, também formada em Edificações.
                 </div>
                 <div className={styles.feedImg3}></div>
                 <div className={styles.feedText}>
-                    gfhjgyukuykjgykjg
+                    Somos a equipe RC Arkhi Design, uma empresa voltada em projetos de design de interiores, podendo elaborar móveis planejados, iluminação e decoração de ambientes.
+          Nosso time é composto por dois fundadores e integrantes: Christian Figueiredo, formado no curso Técnico de Edificações – que, inclusive, dá aula de Sketchup e Vray - e Raíssa de Sá, também formada em Edificações.
                 </div>
                 <div className={styles.feedImg4}></div>
                 <div className={styles.feedText}>
-                    gfhjgyukuykjgykjg
+                    Somos a equipe RC Arkhi Design, uma empresa voltada em projetos de design de interiores, podendo elaborar móveis planejados, iluminação e decoração de ambientes.
+          Nosso time é composto por dois fundadores e integrantes: Christian Figueiredo, formado no curso Técnico de Edificações – que, inclusive, dá aula de Sketchup e Vray - e Raíssa de Sá, também formada em Edificações.
                 </div>
                 <div className={styles.feedImg5}></div>
                 <div className={styles.feedText}>
-                    gfhjgyukuykjgykjg
+                    Somos a equipe RC Arkhi Design, uma empresa voltada em projetos de design de interiores, podendo elaborar móveis planejados, iluminação e decoração de ambientes.
+          Nosso time é composto por dois fundadores e integrantes: Christian Figueiredo, formado no curso Técnico de Edificações – que, inclusive, dá aula de Sketchup e Vray - e Raíssa de Sá, também formada em Edificações.
                 </div>
                 <div className={styles.feedImg6}></div>
                 <div className={styles.feedText}>
-                    gfhjgyukuykjgykjg
+                    Somos a equipe RC Arkhi Design, uma empresa voltada em projetos de design de interiores, podendo elaborar móveis planejados, iluminação e decoração de ambientes.
+          Nosso time é composto por dois fundadores e integrantes: Christian Figueiredo, formado no curso Técnico de Edificações – que, inclusive, dá aula de Sketchup e Vray - e Raíssa de Sá, também formada em Edificações.
                 </div>
                 <div className={styles.feedImg7}></div>
                 <div className={styles.feedText}>
-                    gfhjgyukuykjgykjg
+                    Somos a equipe RC Arkhi Design, uma empresa voltada em projetos de design de interiores, podendo elaborar móveis planejados, iluminação e decoração de ambientes.
+          Nosso time é composto por dois fundadores e integrantes: Christian Figueiredo, formado no curso Técnico de Edificações – que, inclusive, dá aula de Sketchup e Vray - e Raíssa de Sá, também formada em Edificações.
                 </div>
                 <div className={styles.feedImg8}></div>
                 <div className={styles.feedText}>
-                    gfhjgyukuykjgykjg
+                    Somos a equipe RC Arkhi Design, uma empresa voltada em projetos de design de interiores, podendo elaborar móveis planejados, iluminação e decoração de ambientes.
+          Nosso time é composto por dois fundadores e integrantes: Christian Figueiredo, formado no curso Técnico de Edificações – que, inclusive, dá aula de Sketchup e Vray - e Raíssa de Sá, também formada em Edificações.
                 </div>
             </div>
             <div className={styles.picture}>
